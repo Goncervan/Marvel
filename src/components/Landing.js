ba808635e9c2183ff688735b6046e1ad8bd5092b
Goncervan <gonfedecer@gmail.com>
2022-02-15 18:59:53 -0300

    Mejorado el landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,10 +5,12 @@ import s from './Styles/Landing.module.css'
 export default function Landing() {
     return (
         <div className={s.container}>
+            <img className={s.img} src="" alt="Marvel logo" />
             <div className={s.cont}>
-                {/* <img className={s.img} src="https://marcas-logos.net/wp-content/uploads/2020/02/Marvel-Comics-Logo-1983.png" alt="Marvel logo" /> */}
-                <h1 className={s.h1}>MARVEL</h1>
-                <h2 className={s.title}>Api By Gonzalo Cervan</h2>
+                <div className={s.divTitle}>
+                    <h1 className={s.h1}>MARVEL</h1>
+                    <h2 className={s.title}>Api By Gonzalo Cervan</h2>
+                </div>
                 <div className={s.containerBtn}>
                     <Link className={s.link} to="/series"><button className={s.boton}><span>Series</span></button></Link>
                     <Link className={s.link} to="/characters"><button className={s.boton}><span>Characters</span></button></Link>
